d0eab6e5211d311086e787f59235ba395f6d6c4e
domster704 <domster704@mail.ru>
2024-09-21 13:42:44 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-docker docker build -t portfolio:latest . && > docker run -d -it -p 8080:80 portfolio:latest --name portfolio-container
+docker build -t portfolio:latest . && > docker run -d -it -p 8080:80 portfolio:latest --name portfolio-container
