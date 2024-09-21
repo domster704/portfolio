c2e237b8b344b5b0d1e6cb1a70f70aaf20ea76de
domster704 <domster704@mail.ru>
2024-09-21 14:23:01 +0300

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ sudo git reset HEAD --hard && sudo git pull
 
 CONTAINER_NAME="portfolio-container"
 
-docker stop $CONTAINER_NAME &&
- docker rm $CONTAINER_NAME &&
-  docker build -t portfolio:latest . &&
-    docker run -it -p 8080:80 -d --name $CONTAINER_NAME portfolio:latest
+docker stop $CONTAINER_NAME && docker rm $CONTAINER_NAME
+
+docker build -t portfolio:latest . && docker run -it -p 8080:80 -d --name $CONTAINER_NAME portfolio:latest
