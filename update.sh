4e2e9bb67787a84101c29ab499bdc7304c2b3474
domster704 <domster704@mail.ru>
2025-01-20 16:30:48 +0300

    update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 git reset --hard HEAD
 git pull origin master
-cd portfolio-cms && git reset --HARD && git pull origin master
-cd ../portfolio-react && git reset --hard && git pull origin master
+cd portfolio-cms && git reset --hard HEAD && git pull origin master
+cd ../portfolio-react && git reset --hard HEAD && git pull origin master
 cd ../
 
 docker compose down &&
